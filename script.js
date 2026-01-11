ae5b67e75224e68758df5edba77cf53780ff3e1c
Sulton <sultonorinboyev19@gmail.com>
2026-01-11 10:52:25 +0500

    xatoliklar to'g'rilandi

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('mobile-menu');
-const nav = document.getElementById('nav-links');
-
-menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
